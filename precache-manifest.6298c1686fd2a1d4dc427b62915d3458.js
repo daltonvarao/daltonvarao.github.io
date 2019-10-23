f3928d3fcb69aa50dac16c08771ce576002a76dd
daltonfelipe <daltonphellipe@gmail.com>
2019-10-23 19:47:22 -0300

    Updates

---
+++ precache-manifest.6298c1686fd2a1d4dc427b62915d3458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1e99b35c4e2b13f8504a5b5a3fdb81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c136e7605b1439496dec",
+    "url": "/static/css/main.b8ab16a0.chunk.css"
+  },
+  {
+    "revision": "d8026fc763ca2dce1af0",
+    "url": "/static/js/2.126e4470.chunk.js"
+  },
+  {
+    "revision": "c136e7605b1439496dec",
+    "url": "/static/js/main.31b5981c.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  }
+]);
